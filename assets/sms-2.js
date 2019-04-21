b836f91077d450ef60182b013a70ce5893e8609b
TRyON-Design <tryon-design@mac-mini-de-tryon-design.home>
2019-04-21 18:23:06 +0200

    update change ready to play

---
+++ assets/sms-2.js
@@ -0,0 +1,201 @@
+(function () {
+
+  // L'ANIMATION :
+
+  // ---
+  var text1 = "texte dnumero 1", /*"Quand tu sortiras de ta bibliotheque cherie, sache que tu dors dans le salon. Y a un de mes eleves qui squatte ta chambre !",*/
+      text2 = "Combien ils te payent, deja, pour pas dormir ?",
+      keyboard,
+      restartBtn,
+      message,
+      theBody,
+      step=1,
+      tmp=0,
+      actualText = text1,
+      nextStep=false;
+  // ---
+  window.onload = function() {
+    keyboard = document.querySelector("#keyboard-button");
+    sendBtn = document.querySelector("#send-button");
+    message = document.querySelector("#message p");
+    restartBtn = document.querySelector("#restart-button");
+    theBody = document.querySelector("body");
+
+    keyboard.addEventListener("touchend", keyboardHandler, false);
+    sendBtn.addEventListener("touchend", sendHandler, false);
+    restartBtn.addEventListener("touchend", restartHandler, false);
+
+    featToScrean();
+    init();
+  }
+
+  function init(){
+    step=1,
+    tmp=0,
+    actualText = text1,
+    nextStep=false;
+    message.innerHTML = "|";
+    theBody.classList.add("step1");
+    theBody.classList.remove("step2");
+    theBody.classList.remove("sended");
+    keyboard.classList.remove("hidden");
+  }
+
+  function restartHandler(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log("FUCK !");
+    console.log("nextStep : "+nextStep);
+    console.log("step : "+step);
+
+
+    if(!nextStep){
+      init();
+      theBody.classList.remove("step1");
+      theBody.classList.remove("step2");
+      theBody.classList.add("step1");
+    }else{
+      switch (step) {
+        case 1:
+          step=2;
+          tmp=0;
+          actualText = text2;
+          nextStep=false;
+          theBody.classList.remove("step1");
+          theBody.classList.remove("sended");
+          theBody.classList.add("step2");
+          keyboard.classList.remove("hidden");
+          break;
+        case 2:
+          init();
+          break;
+        default:
+
+      }
+    }
+
+  }
+
+
+  function keyboardHandler(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    var totalCarc = actualText.length;
+    if(tmp<totalCarc+1){
+      tmp++;
+      var newText = actualText.substr(0, tmp)+"|";
+      message.innerHTML = newText;
+    }else{
+      keyboard.classList.add("hidden");
+      nextStep = true;
+    }
+  }
+
+  function sendHandler(e){
+    e.preventDefault();
+    e.stopPropagation();
+
+    if(nextStep){
+      message.innerHTML = "|";
+      theBody.classList.add("sended");
+    }
+
+  }
+
+
+
+  function featToScrean(){
+    var availableHeight = getWindowHeight(),
+        availableWidth = getWindowWidth(),
+        contentWidth = 375,
+        contentHeight = 667,
+        scale = Math.min(
+          availableWidth / contentWidth,
+          availableHeight / contentHeight),
+        theBody,
+        translatex;
+    translatex = -((availableWidth - availableWidth*scale)/2 + 3);
+    theBody = document.querySelector("body");
+    if(scale===1){
+        translatex = 0;
+    }
+    theBody.style.transform="scale("+scale+") translateX("+translatex+"px)";
+  }
+
+  // RECUPERATION WIDTH - HEIGHT DU DEVICE :
+    function getWindowHeight() {
+      var windowHeight=0;
+      if (typeof(window.innerHeight)=='number') {
+          windowHeight=window.innerHeight;
+      } else {
+          if (document.documentElement&& document.documentElement.clientHeight) {
+              windowHeight = document.documentElement.clientHeight;
+          } else {
+              if (document.body&&document.body.clientHeight) {
+                  windowHeight=document.body.clientHeight;
+              }
+          }
+      }
+      return windowHeight;
+  }
+  function getWindowWidth() {
+   var windowWidth=0;
+   if (typeof(window.innerWidth)=='number') {
+    windowWidth=window.innerWidth;
+      } else {
+    if (document.documentElement&& document.documentElement.clientWidth) {
+     windowWidth = document.documentElement.clientWidth;
+          } else {
+     if (document.body&&document.body.clientWidth) {
+      windowWidth=document.body.clientWidth;
+              }
+          }
+      }
+   return windowWidth;
+  }
+
+
+
+
+
+
+
+  // LES WEBSERVICES :
+  //If serviceWorker supports, then register it.
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register('./serviceWorker.js', { scope: "./" }) //setting scope of sw
+    .then(function(registration) {
+      console.info('Service worker is registered!');
+      checkForPageUpdate(registration); // To check if new content is updated or not
+    })
+    .catch(function(error) {
+      console.error('Service worker failed ', error);
+    });
+  }
+
+  // To content update on service worker state change
+  function checkForPageUpdate(registration) {
+    // onupdatefound will fire on first time install and when serviceWorker.js file changes
+    registration.addEventListener("updatefound", function() {
+      // To check if service worker is already installed and controlling the page or not
+      if (navigator.serviceWorker.controller) {
+        var installingSW = registration.installing;
+        installingSW.onstatechange = function() {
+          console.info("Service Worker State :", installingSW.state);
+          switch(installingSW.state) {
+            case 'installed':
+              // Now new contents will be added to cache and old contents will be remove so
+              // this is perfect time to show user that page content is updated.
+              // toast('Site is updated. Refresh the page.', 5000);
+              console.info("Site is updated. Refresh the page.");
+              break;
+            case 'redundant':
+              throw new Error('The installing service worker became redundant.');
+          }
+        }
+      }
+    });
+  }
+})();
---
+++ serviceWorker.js
@@ -0,0 +1,222 @@
+//Cache polyfil to support cacheAPI in all browsers
+importScripts('./assets/cache-polyfill.js');
+
+var cacheName = 'SAM-iPhone6-sms-2-v0';
+
+//Files to save in cache
+var files = [
+  './',
+  './index.html?utm=homescreen', //SW treats query string as new request
+  './assets',
+  './assets/img',
+  './assets/img/thumb-SCREEN.jpg',
+  './assets/img/icon-144.png',
+  './assets/img/icon-192.png',
+  './assets/img/icon-512.png',
+  './assets/img/sms-sam-2319.jpg',
+  './assets/img/arraw.gif',
+  './assets/img/icon.svg',
+  './assets/reset.css',
+  './assets/main.css',
+  './assets/main.js',
+  './assets/manifest.json'
+];
+
+//Adding `install` event listener
+self.addEventListener('install', (event) => {
+  // console.info('Event: Install');
+
+  event.waitUntil(
+    caches.open(cacheName)
+    .then((cache) => {
+      //[] of files to cache & if any of the file not present `addAll` will fail
+      return cache.addAll(files)
+      .then(() => {
+        // console.info('All files are cached');
+        return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+      })
+      .catch((error) =>  {
+        // console.error('Failed to cache', error);
+      })
+    })
+  );
+});
+
+/*
+  FETCH EVENT: triggered for every request made by index page, after install.
+*/
+
+//Adding `fetch` event listener
+self.addEventListener('fetch', (event) => {
+  // console.info('Event: Fetch');
+
+  var request = event.request;
+
+  //Tell the browser to wait for newtwork request and respond with below
+  event.respondWith(
+    //If request is already in cache, return it
+    caches.match(request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      // // Checking for navigation preload response
+      // if (event.preloadResponse) {
+      //   console.info('Using navigation preload');
+      //   return response;
+      // }
+
+      //if request is not cached or navigation preload response, add it to cache
+      return fetch(request).then((response) => {
+        var responseToCache = response.clone();
+        caches.open(cacheName).then((cache) => {
+            cache.put(request, responseToCache).catch((err) => {
+              // console.warn(request.url + ': ' + err.message);
+            });
+          });
+
+        return response;
+      });
+    })
+  );
+});
+
+/*
+  ACTIVATE EVENT: triggered once after registering, also used to clean up caches.
+*/
+
+//Adding `activate` event listener
+self.addEventListener('activate', (event) => {
+  // console.info('Event: Activate');
+
+  //Navigation preload is help us make parallel request while service worker is booting up.
+  //Enable - chrome://flags/#enable-service-worker-navigation-preload
+  //Support - Chrome 57 beta (behing the flag)
+  //More info - https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
+
+  // Check if navigationPreload is supported or not
+  // if (self.registration.navigationPreload) {
+  //   self.registration.navigationPreload.enable();
+  // }
+  // else if (!self.registration.navigationPreload) {
+  //   console.info('Your browser does not support navigation preload.');
+  // }
+
+  //Remove old and unwanted caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache); //Deleting the old cache (cache v1)
+          }
+        })
+      );
+    })
+    .then(function () {
+      // console.info("Old caches are cleared!");
+      // To tell the service worker to activate current one
+      // instead of waiting for the old one to finish.
+      return self.clients.claim();
+    })
+  );
+});
+//Adding `install` event listener
+self.addEventListener('install', (event) => {
+  // console.info('Event: Install');
+
+  event.waitUntil(
+    caches.open(cacheName)
+    .then((cache) => {
+      //[] of files to cache & if any of the file not present `addAll` will fail
+      return cache.addAll(files)
+      .then(() => {
+        // console.info('All files are cached');
+        return self.skipWaiting(); //To forces the waiting service worker to become the active service worker
+      })
+      .catch((error) =>  {
+        // console.error('Failed to cache', error);
+      })
+    })
+  );
+});
+
+/*
+  FETCH EVENT: triggered for every request made by index page, after install.
+*/
+
+//Adding `fetch` event listener
+self.addEventListener('fetch', (event) => {
+  // console.info('Event: Fetch');
+
+  var request = event.request;
+
+  //Tell the browser to wait for newtwork request and respond with below
+  event.respondWith(
+    //If request is already in cache, return it
+    caches.match(request).then((response) => {
+      if (response) {
+        return response;
+      }
+
+      // // Checking for navigation preload response
+      // if (event.preloadResponse) {
+      //   console.info('Using navigation preload');
+      //   return response;
+      // }
+
+      //if request is not cached or navigation preload response, add it to cache
+      return fetch(request).then((response) => {
+        var responseToCache = response.clone();
+        caches.open(cacheName).then((cache) => {
+            cache.put(request, responseToCache).catch((err) => {
+              // console.warn(request.url + ': ' + err.message);
+            });
+          });
+
+        return response;
+      });
+    })
+  );
+});
+
+/*
+  ACTIVATE EVENT: triggered once after registering, also used to clean up caches.
+*/
+
+//Adding `activate` event listener
+self.addEventListener('activate', (event) => {
+  // console.info('Event: Activate');
+
+  //Navigation preload is help us make parallel request while service worker is booting up.
+  //Enable - chrome://flags/#enable-service-worker-navigation-preload
+  //Support - Chrome 57 beta (behing the flag)
+  //More info - https://developers.google.com/web/updates/2017/02/navigation-preload#the-problem
+
+  // Check if navigationPreload is supported or not
+  // if (self.registration.navigationPreload) {
+  //   self.registration.navigationPreload.enable();
+  // }
+  // else if (!self.registration.navigationPreload) {
+  //   console.info('Your browser does not support navigation preload.');
+  // }
+
+  //Remove old and unwanted caches
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== cacheName) {
+            return caches.delete(cache); //Deleting the old cache (cache v1)
+          }
+        })
+      );
+    })
+    .then(function () {
+      // console.info("Old caches are cleared!");
+      // To tell the service worker to activate current one
+      // instead of waiting for the old one to finish.
+      return self.clients.claim();
+    })
+  );
+});
