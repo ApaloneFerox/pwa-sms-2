948361cde22905110a986041ac2483566c060b20
TRyON-Design <tryon-design@mac-mini-de-tryon-design.home>
2019-04-22 00:49:56 +0200

    manifest & iPhone offline OK

--- assets/sms-2.js
+++ assets/sms-2.js
@@ -1,4 +1,21 @@
-(function () {
+document.addEventListener(
+  'DOMContentLoaded',
+   (event) => {
+      //the event occurred
+      keyboard = document.querySelector("#keyboard-button");
+      sendBtn = document.querySelector("#send-button");
+      message = document.querySelector("#message p");
+      restartBtn = document.querySelector("#restart-button");
+      theBody = document.querySelector("body");
+
+      keyboard.addEventListener("touchend", keyboardHandler, false);
+      sendBtn.addEventListener("touchend", sendHandler, false);
+      restartBtn.addEventListener("touchend", restartHandler, false);
+
+      featToScrean();
+      init();
+  }
+)
 
   // L'ANIMATION :
 
@@ -14,20 +31,7 @@
       actualText = text1,
       nextStep=false;
   // ---
-  window.onload = function() {
-    keyboard = document.querySelector("#keyboard-button");
-    sendBtn = document.querySelector("#send-button");
-    message = document.querySelector("#message p");
-    restartBtn = document.querySelector("#restart-button");
-    theBody = document.querySelector("body");
 
-    keyboard.addEventListener("touchend", keyboardHandler, false);
-    sendBtn.addEventListener("touchend", sendHandler, false);
-    restartBtn.addEventListener("touchend", restartHandler, false);
-
-    featToScrean();
-    init();
-  }
 
   function init(){
     step=1,
@@ -151,47 +155,3 @@
       }
    return windowWidth;
   }
-
-
-
-
-
-
-
-  // LES WEBSERVICES :
-  //If serviceWorker supports, then register it.
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register('./serviceWorker.js', { scope: "./" }) //setting scope of sw
-    .then(function(registration) {
-      console.info('Service worker is registered!');
-      checkForPageUpdate(registration); // To check if new content is updated or not
-    })
-    .catch(function(error) {
-      console.error('Service worker failed ', error);
-    });
-  }
-
-  // To content update on service worker state change
-  function checkForPageUpdate(registration) {
-    // onupdatefound will fire on first time install and when serviceWorker.js file changes
-    registration.addEventListener("updatefound", function() {
-      // To check if service worker is already installed and controlling the page or not
-      if (navigator.serviceWorker.controller) {
-        var installingSW = registration.installing;
-        installingSW.onstatechange = function() {
-          console.info("Service Worker State :", installingSW.state);
-          switch(installingSW.state) {
-            case 'installed':
-              // Now new contents will be added to cache and old contents will be remove so
-              // this is perfect time to show user that page content is updated.
-              // toast('Site is updated. Refresh the page.', 5000);
-              console.info("Site is updated. Refresh the page.");
-              break;
-            case 'redundant':
-              throw new Error('The installing service worker became redundant.');
-          }
-        }
-      }
-    });
-  }
-})();
